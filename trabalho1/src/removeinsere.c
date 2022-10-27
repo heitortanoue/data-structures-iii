a52b729676be0b59361c5d60b16752f538f14193
Heitor Tanoue <heitortanoue@usp.br>
2022-10-26 22:41:41 -0300

    certo

--- trabalho1/src/removeinsere.c
+++ trabalho1/src/removeinsere.c
@@ -36,7 +36,7 @@ int entradaDados(Registro *reg){
 
     
     for (int i = 0; i < 7; i++){
-        scan_quote_string(dados_string[i]); //le ate espaço ou \n removendo as aspas se tiver
+        scanTeclado(dados_string[i]); //le ate espaço ou \n removendo as aspas se tiver
         if(!strcmp(dados_string[i], "NULO")){
             strcpy(dados_string[i], "");
         }
